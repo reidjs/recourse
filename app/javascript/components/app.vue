b9e9a8dcaffc7b147d20438a2db2f1d011f01864
Christopher Bigelow <chrisbigelow25@gmail.com>
2018-01-27 17:03:20 -0800

    signup and login forms

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -1,22 +1,100 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+<transition name="fade" mode="out-in">
+    <main class='main-content'>
+      <transition name="fade" mode="out-in">
+        <router-view></router-view>
+      </transition>
+    </main>
+</transition>
   </div>
+
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
-    }
-  }
-}
-</script>
+  import Vue from 'vue';
+  import VueRouter from 'vue-router';
+  // import SessionForm from './components/session_form'
 
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
+  // import IndexComponent from './components/index_component'
+  // import SiteShowComponent from './components/site_show_component';
+  import LoginForm from './login_form'
+  import SignupForm from './signup_form'
+  import Vuex from 'vuex';
+  // import Vuetify from 'vuetify';
+
+
+  
+  // import('../../app/assets/stylesheets/vuetify.css');
+  // Vue.use(Vuetify);
+  Vue.use(VueRouter);
+  Vue.use(Vuex);
+ 
+
+  const config = {
+    errorBagName: 'frontendErrors', // change if property conflicts
+  }
+
+  //  Vue.use(VeeValidate, config);
+
+  import { store } from '../store';
+
+
+  const routes = [
+    
+    { path: '/', component: SignupForm, meta: { requiresUnAuth: true} },
+    // { path: '/', component: IndexComponent,meta: { requiresAuth: true }},
+    { path: '/login', component: LoginForm, meta: { requiresUnAuth: true} },
+    { path: '/signup', component: SignupForm, meta: { requiresUnAuth: true} }
+]
+
+  const router = new VueRouter({
+    // mode: 'history',
+    routes // short for `routes: routes`
+  })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+
+    if (!store.state.session.currentUser ) {
+      next({
+        path: '/signup',
+        query: { redirect: to.fullPath }
+      })
+    } else {
+      next()
+    }
+  }else if(to.matched.some(record => record.meta.requiresUnAuth)){
+      if (store.state.session.currentUser ) {
+            next({
+              path: '/',
+              query: { redirect: to.fullPath }
+            })
+          } else {
+            next()
+          }
+  }  else {
+    next()
+  }
+})
+
+
+
+ export default {
+  name: 'app',
+  router,
+  computed: {
+    listings: function() {
+      return this.$store.getters.listings
+    },
+    session: function() {
+      return this.$store.getters.session
+    }
+  },
+  store
+  // components:{
+  //   Sidebar
+  // }
 }
-</style>
+
+</script>
---
+++ app/javascript/components/login_form.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="auth-form">
+    <div class="auth-inner-wrapper">
+      <h1> Welcome, log in to get started</h1>
+      <ul id="error-list">
+         <li class="error-msg">
+          {{errors}}
+        </li>
+      </ul>
+      <div class="form-name form">
+        <label class="label-username">Orginization Name </label>
+        <input type="text" v-on:keyup="clearErrors" @keydown.enter="login" v-model="user.username">
+      </div>
+      <div class="form-password form">
+        <label class="label-password">Password </label>
+        <div class="pw-submit">
+          <input type="password" v-on:keyup="clearErrors" @keydown.enter="login" v-model="user.password">
+          <button v-on:click="login">LOG IN</button>    
+        </div>
+      </div>
+      <div id="signup-button">
+        <p class="account-prompt">Don't have an account?
+          <router-link v-on:click.native="clearErrors" to="/signup">Click Here</router-link> to create one!
+        </p> 
+      </div>
+    </div>
+  </div>  
+</template>
+
+
+<script>
+  import axios from 'axios';
+  export default {
+    name: 'login-form',
+    data() {
+      return{
+        user: {
+          username: '',
+          password: ''
+        }
+      }
+    },
+    computed:{
+      errors () {
+        return this.$store.getters.errors
+      }
+    },
+    methods:{
+      login: function() {
+        let user = {
+          "user": {
+            "username": this.user.username,
+            "password": this.user.password
+          } 
+        }
+        this.$store.dispatch('logCurrentUserIn',user).then( ()=>{
+        this.$router.push('/')})
+      },
+      loginDemoUser: function() {
+        let user = {
+          "user": {
+            "username": 'DemoUser',
+            "password": 'asdfasdf'
+          } 
+        }
+        this.$store.dispatch('logCurrentUserIn',user).then( ()=>{
+        this.$router.push('/')})
+      },
+      clearErrors: function() {
+        this.$store.dispatch('clearErrors');
+      }
+    },
+
+  }
+</script>
