29bb87fdd9a6f0bf0401b3bcc6f6a769a60fde13
Reid Sherman <reidsherman@gmail.com>
2018-01-28 09:57:26 -0800

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,8 +21,8 @@ user_b = User.create(
 )
 
 list1 = Listing.new(
-  list_name: 'Baby food - 1 pallet',
-  description: 'tbd',
+  list_name: 'Spam - 1 pallet',
+  description: 'We just received a MASSIVE shipment of spam. We have no idea what to do with them.',
   user_id: User.first.id,
   end_date: Date.tomorrow.noon,
   list_url: "http://res.cloudinary.com/dmzulpcul/image/upload/v1517159788/spam-cans_xzvymc.jpg"
@@ -31,40 +31,40 @@ list1 = Listing.new(
 list1.save!
 
 Listing.create(
-  list_name: 'School Supplies - 200 kits',
-  description: 'tbd',
+  list_name: 'One bushel of fresh tomatoes',
+  description: 'Kids at the youth alliance are not too stoked about tomatoes. Please take these off our hands',
   user_id: user_a.id,
-  end_date: Date.tomorrow.noon,
+  end_date: Date.tomorrow.tomorrow.noon,
   list_url: "http://res.cloudinary.com/dmzulpcul/image/upload/v1517159788/1371616351150_efjphs.jpg"
 )
 
 Listing.create(
-  list_name: 'Hygiene Products - 300 kits',
-  description: 'tbd',
+  list_name: 'Used Clothing',
+  description: 'Buffalo exchange just dropped off a big shipment of large mens clothing.',
   user_id: User.last.id,
-  end_date: Date.tomorrow.noon,
+  end_date: Date.tomorrow.midnight,
   list_url: "http://res.cloudinary.com/dmzulpcul/image/upload/v1517159788/Jackets-Grade-B_pgx3gd.jpg"
 )
 
 Listing.create(
-  list_name: 'Clothing (toddler onesies) - 10',
-  description: 'tbd',
+  list_name: 'First Aid Kit',
+  description: 'We gutted the storage room and found a bunch of first aid kits.',
   user_id: user_a.id,
-  end_date: Date.tomorrow.noon,
+  end_date: Date.tomorrow.tomorrow.midnight,
   list_url: "http://res.cloudinary.com/dmzulpcul/image/upload/v1517159788/d46ca63f8e053463b0cf77d648dabd09_lj0bkr.jpg"
 )
 
 Listing.create(
-  list_name: 'Bedding (single) - 20 sets',
-  description: 'tbd',
+  list_name: 'Toothpaste',
+  description: 'We did a costco run and ended up with way too much toothpaste. Up for grabs!',
   user_id: User.last.id,
   end_date: Date.tomorrow.noon,
   list_url: "http://res.cloudinary.com/dmzulpcul/image/upload/v1517159788/himalaya_dentalcream-sparly-white_1280x960_yazhhc.jpg"
 )
 
 Listing.create(
-  list_name: 'Sleeping Bags - 30',
-  description: 'tbd',
+  list_name: 'Canned Goods -- 30 cans!',
+  description: 'After a local school drive we had waaaay too many cans left over. Start the bidding',
   user_id: User.last.id,
   end_date: Date.tomorrow.noon,
   list_url: "http://res.cloudinary.com/dmzulpcul/image/upload/v1517159788/Bags_canned_goods_eehdp7.jpg"
