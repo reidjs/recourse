5434931a921b23be6686417e8b623d048cf6bb52
Christopher Bigelow <chrisbigelow25@gmail.com>
2018-01-27 20:33:32 -0800

    fixed merge conflicts

---
+++ app/javascript/components/countdown.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="block">
+        <p class="digit">{{ days | two_digits }}</p>
+        <p class="text">Days</p>
+    </div>
+    <div class="block">
+        <p class="digit">{{ hours | two_digits }}</p>
+        <p class="text">Hours</p>
+    </div>
+    <div class="block">
+        <p class="digit">{{ minutes | two_digits }}</p>
+        <p class="text">Minutes</p>
+    </div>
+    <div class="block">
+        <p class="digit">{{ seconds | two_digits }}</p>
+        <p class="text">Seconds</p>
+    </div>
+</template>
+<script>
+export default {
+
+    ready() {
+        window.setInterval(() => {
+            this.now = Math.trunc((new Date()).getTime() / 1000);
+        },1000);
+    },
+
+    props : {
+        date : {
+            type: Number,
+            coerce: str => Math.trunc(Date.parse(str) / 1000)
+        }
+    },
+
+    data() {
+        return {
+            now: Math.trunc((new Date()).getTime() / 1000)
+        }
+    },
+
+    computed: {
+        seconds() {
+            return (this.date - this.now) % 60;
+        },
+
+        minutes() {
+            return Math.trunc((this.date - this.now) / 60) % 60;
+        },
+
+        hours() {
+            return Math.trunc((this.date - this.now) / 60 / 60) % 24;
+        },
+
+        days() {
+            return Math.trunc((this.date - this.now) / 60 / 60 / 24);
+        }
+    }
+}
+
+</script>
