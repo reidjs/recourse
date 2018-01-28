9f68fbb270478c3cd14c6c41a27f635f2655c954
David Bodow <dbodow@gmail.com>
2018-01-27 16:24:06 -0800

    add mock wallet in rails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # DB: added so I could progress without yarn
+  config.webpacker.check_yarn_integrity = false
 end
